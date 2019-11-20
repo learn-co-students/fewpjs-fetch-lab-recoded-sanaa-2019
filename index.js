74c056aff5e5fba3ce3ea93129bfa41515cd061f
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-11-20 05:22:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-function fetchBooks() {
-
-}
+const fetchBooks = () =>
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(data => renderBooks(data));
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector("main");
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function() {
+  fetchBooks();
+});
