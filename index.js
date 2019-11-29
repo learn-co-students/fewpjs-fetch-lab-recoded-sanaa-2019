3916dfcb930bd9dff03b82c7fc6de6924142d0f7
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-11-29 17:08:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
+const mainForTitles = document.querySelector("main");
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books").then(res => res.json()).then(data => renderBooks(data));
 }
 
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+function renderBooks(data) {
+  for(let i = 0; i < data.length; i++){
+    let h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${data[i].name}</h2>`;
+    mainForTitles.appendChild(h2)
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
-})
